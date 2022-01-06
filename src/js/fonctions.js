62c8a16d5f3bdd861409c1080d10a73d523cd400
gros-eliot <eliot.gros@edu.univ-fcomte.fr>
2022-01-07 00:54:40 +0100

    frise terminée

---
+++ src/js/fonctions.js
@@ -0,0 +1,160 @@
+/*Animation frise1*/
+function montrerFrise1() {
+    return anime({
+        targets: '.frise__legende1',
+        translateY  : -100,
+        autoplay: false,
+        duration: 300,
+        opacity: 1,
+        easing: 'easeInOutSine'
+    }).play;
+}
+
+function cacherFrise1() {
+    return anime({
+        targets: '.frise__legende1',
+        translateY: 0,
+        autoplay: false,
+        duration: 300,
+        opacity: 0,
+        easing: 'easeInOutSine'
+    }).play;
+}
+
+/*Animation frise2*/
+function montrerFrise2() {
+    return anime({
+        targets: '.frise__legende2',
+        translateY  : -100,
+        autoplay: false,
+        duration: 300,
+        opacity: 1,
+        easing: 'easeInOutSine'
+    }).play;
+}
+
+function cacherFrise2() {
+    return anime({
+        targets: '.frise__legende2',
+        translateY: 0,
+        autoplay: false,
+        duration: 300,
+        opacity: 0,
+        easing: 'easeInOutSine'
+    }).play;
+}
+
+/*Animation frise3*/
+function montrerFrise3() {
+    return anime({
+        targets: '.frise__legende3',
+        translateY  : -100,
+        autoplay: false,
+        duration: 300,
+        opacity: 1,
+        easing: 'easeInOutSine'
+    }).play;
+}
+
+function cacherFrise3() {
+    return anime({
+        targets: '.frise__legende3',
+        translateY: 0,
+        autoplay: false,
+        duration: 300,
+        opacity: 0,
+        easing: 'easeInOutSine'
+    }).play;
+}
+
+/*Animation frise4*/
+function montrerFrise4() {
+    return anime({
+        targets: '.frise__legende4',
+        translateY  : -100,
+        autoplay: false,
+        duration: 300,
+        opacity: 1,
+        easing: 'easeInOutSine'
+    }).play;
+}
+
+function cacherFrise4() {
+    return anime({
+        targets: '.frise__legende4',
+        translateY: 0,
+        autoplay: false,
+        duration: 300,
+        opacity: 0,
+        easing: 'easeInOutSine'
+    }).play;
+}
+
+/*Animation frise5*/
+function montrerFrise5() {
+    return anime({
+        targets: '.frise__legende5',
+        translateY  : -100,
+        autoplay: false,
+        duration: 300,
+        opacity: 1,
+        easing: 'easeInOutSine'
+    }).play;
+}
+
+function cacherFrise5() {
+    return anime({
+        targets: '.frise__legende5',
+        translateY: 0,
+        autoplay: false,
+        duration: 300,
+        opacity: 0,
+        easing: 'easeInOutSine'
+    }).play;
+}
+
+/*Animation frise6*/
+function montrerFrise6() {
+    return anime({
+        targets: '.frise__legende6',
+        translateY  : -100,
+        autoplay: false,
+        duration: 300,
+        opacity: 1,
+        easing: 'easeInOutSine'
+    }).play;
+}
+
+function cacherFrise6() {
+    return anime({
+        targets: '.frise__legende6',
+        translateY: 0,
+        autoplay: false,
+        duration: 300,
+        opacity: 0,
+        easing: 'easeInOutSine'
+    }).play;
+}
+
+/*Animation frise7*/
+function montrerFrise7() {
+    return anime({
+        targets: '.frise__legende7',
+        translateY  : -100,
+        autoplay: false,
+        duration: 300,
+        opacity: 1,
+        easing: 'easeInOutSine'
+    }).play;
+}
+
+function cacherFrise7() {
+    return anime({
+        targets: '.frise__legende7',
+        translateY: 0,
+        autoplay: false,
+        duration: 300,
+        opacity: 0,
+        easing: 'easeInOutSine'
+    }).play;
+}
